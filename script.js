3fc94657412d53712406dc96bd46df9af50d4aad
anewm72 <64477719+anewm72@users.noreply.github.com>
2023-06-29 12:59:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-audit.style.display = 'none';
-checkmaterials.style.display = 'none';
-
-if (document.URL.includes("login.html")) {
+function changeLogin() {
     var login = document.getElementById("loginType");
     var selectedValue = login.options[login.selectedIndex].value;
     if (selectedValue == "NHS") {
-      audit.style.display = 'block';
+      audit.style.display = 'inline-block';
+      checkmaterials.style.display = 'none';
     }
     if (selectedValue == "MedTech") {
-      checkmaterials.style.display = 'block';
+      checkmaterials.style.display = 'inline-block';
+      audit.style.display = 'none';
+    }
+    if (selectedValue == "null") {
+      audit.style.display = 'none';
+      checkmaterials.style.display = 'none';
     }
 }
